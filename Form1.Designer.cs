d7b6f8e62996ccb4f12ed67e44bf7caecc282385
sks <ssurovneva@mail.ru>
2023-04-04 21:37:35 +0300

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,151 @@
+﻿namespace machine_treatment_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer_1 = new System.Windows.Forms.NumericUpDown();
+            this.button_start = new System.Windows.Forms.Button();
+            this.button_stop = new System.Windows.Forms.Button();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.label_calend = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.timer_1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(112, 51);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "seconds";
+            // 
+            // timer_1
+            // 
+            this.timer_1.Location = new System.Drawing.Point(237, 51);
+            this.timer_1.Maximum = new decimal(new int[] {
+            20,
+            0,
+            0,
+            0});
+            this.timer_1.Name = "timer_1";
+            this.timer_1.Size = new System.Drawing.Size(120, 26);
+            this.timer_1.TabIndex = 1;
+            this.timer_1.Value = new decimal(new int[] {
+            1,
+            0,
+            0,
+            0});
+            // 
+            // button_start
+            // 
+            this.button_start.BackColor = System.Drawing.SystemColors.Highlight;
+            this.button_start.Font = new System.Drawing.Font("MV Boli", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_start.Location = new System.Drawing.Point(55, 155);
+            this.button_start.Name = "button_start";
+            this.button_start.Size = new System.Drawing.Size(142, 84);
+            this.button_start.TabIndex = 2;
+            this.button_start.Text = "START";
+            this.button_start.UseVisualStyleBackColor = false;
+            this.button_start.Click += new System.EventHandler(this.button_start_Click);
+            // 
+            // button_stop
+            // 
+            this.button_stop.BackColor = System.Drawing.Color.Aquamarine;
+            this.button_stop.Font = new System.Drawing.Font("MV Boli", 8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button_stop.Location = new System.Drawing.Point(237, 153);
+            this.button_stop.Name = "button_stop";
+            this.button_stop.Size = new System.Drawing.Size(146, 86);
+            this.button_stop.TabIndex = 3;
+            this.button_stop.Text = "STOP";
+            this.button_stop.UseVisualStyleBackColor = false;
+            this.button_stop.Click += new System.EventHandler(this.button_stop_Click);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(99, 299);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(227, 26);
+            this.dateTimePicker1.TabIndex = 4;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(464, 179);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 5;
+            this.monthCalendar1.DateChanged += new System.Windows.Forms.DateRangeEventHandler(this.monthCalendar1_DateChanged);
+            // 
+            // label_calend
+            // 
+            this.label_calend.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(255)))));
+            this.label_calend.Location = new System.Drawing.Point(464, 112);
+            this.label_calend.Name = "label_calend";
+            this.label_calend.Size = new System.Drawing.Size(228, 46);
+            this.label_calend.TabIndex = 6;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 625);
+            this.Controls.Add(this.label_calend);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.button_stop);
+            this.Controls.Add(this.button_start);
+            this.Controls.Add(this.timer_1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.timer_1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown timer_1;
+        private System.Windows.Forms.Button button_start;
+        private System.Windows.Forms.Button button_stop;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label label_calend;
+        private System.Windows.Forms.Timer timer2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace machine_treatment_2
+{
+    public partial class Form1 : Form
+    {
+        Timer timer1 = new Timer(); 
+        public Form1()
+        {
+            InitializeComponent();
+            button_stop.Enabled = false;
+            monthCalendar1.DateChanged += monthCalendar1_DateChanged;
+
+            timer1.Tick += new EventHandler(ShowTimer);
+
+            label_calend.Text = DateTime.Now.ToLongTimeString();
+
+            timer2.Tick += new EventHandler(ShowTimer2);
+            timer2.Interval = 500;
+            timer2.Start();
+        }
+
+        private void ShowTimer(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            button_stop.Enabled = false;
+            MessageBox.Show("Timer stopped", "Timer");
+        }
+
+        private void ShowTimer2(object sender, EventArgs e)
+        {
+            label_calend.Text = DateTime.Now.ToLongTimeString();
+        }
+
+        private void button_start_Click(object sender, EventArgs e)
+        {
+            if(timer_1.Value < 0)
+            {
+                MessageBox.Show("Error");
+            }
+            button_stop.Enabled = true;
+            timer1.Interval = Decimal.ToInt32(timer_1.Value) * 1000;
+            timer1.Start();
+        }
+
+        private void button_stop_Click(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            MessageBox.Show("Timer stopped before end of time", "Timer");
+            button_stop.Enabled = false;
+        }
+
+        private void monthCalendar1_DateChanged(object sender, DateRangeEventArgs e)
+        {
+            label_calend.Text = e.Start.ToShortDateString();
+        }
+    }
+}
